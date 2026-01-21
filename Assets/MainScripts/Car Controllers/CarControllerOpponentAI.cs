65653ea7baabb57950a493ee108989c2201ee770
LupusCoffee <96193514+LupusCoffee@users.noreply.github.com>
2026-01-21 15:44:44 +0100

    boingo boingo

--- Assets/MainScripts/Car Controllers/CarControllerOpponentAI.cs
+++ Assets/MainScripts/Car Controllers/CarControllerOpponentAI.cs
@@ -8,6 +8,13 @@ namespace Car
 {
     public class CarControllerOpponentAI : CarController
     {
+        //Variables
+        [Header("Misc")] 
+        [SerializeField]
+        private GameObject TargetPoint;
+        
+        
+        //Methods
         private void Start()
         {
             Debug.Log("CarControllerOpponentAI started");
@@ -16,9 +23,29 @@ namespace Car
         protected override void Update()
         {
             base.Update();
-
-            msteerInput = 1;
-            mthrottleInput = 0.5f;
+            
+            mthrottleInput = 1f;
+            
+            Vector3 dirForward = transform.forward;
+            Debug.DrawLine(transform.position, transform.position + dirForward*1000.0f, Color.red);
+            
+            //find out if point is on left or right side of me
+            Vector3 dirToTarget = TargetPoint.transform.position - transform.position;
+            Debug.DrawLine(transform.position, transform.position + dirToTarget*1000.0f, Color.yellow);
+            
+            float crossProduct = (dirForward.x * dirToTarget.z) - (dirForward.z * dirToTarget.x);
+            if (crossProduct > 0) //dirForward on the right of dirToTarget
+            {
+                msteerInput = -1.0f;
+            }
+            else if (crossProduct < 0) //dirForward on the left of dirToTarget
+            {
+                msteerInput = 1.0f;
+            }
+            else
+            {
+                msteerInput = 0.0f;
+            }
         }
     }
 }
