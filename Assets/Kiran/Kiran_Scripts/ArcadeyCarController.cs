43fb1dc767b583e29020a1e749e9928e216d8723
Kiran George <gaurdian.kiran@gmail.com>
2026-01-21 14:45:43 +0100

    adwa

--- Assets/Kiran/Kiran_Scripts/ArcadeyCarController.cs
+++ Assets/Kiran/Kiran_Scripts/ArcadeyCarController.cs
@@ -6,7 +6,7 @@ namespace Car
     public class ArcadeyCarController : CarController
     {
         [SerializeField] private float mgripDuringLateralMovement = 1.0f;
-        [SerializeField] private float mgrip
+        [SerializeField] private float mgripDuringSidewaysMovement = 4.0f;
         void Update()
         {
             base.Update();
