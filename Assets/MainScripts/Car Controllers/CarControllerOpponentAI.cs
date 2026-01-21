000b1a14489295635f1cc7dcde42ec48ca5f6a59
LupusCoffee <96193514+LupusCoffee@users.noreply.github.com>
2026-01-21 14:41:00 +0100

    yoop

---
+++ Assets/MainScripts/Car Controllers/CarControllerOpponentAI.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Car
+{
+    public class CarControllerOpponentAI : CarController
+    {
+        private void Start()
+        {
+            Debug.Log("CarControllerOpponentAI started");
+        }
+
+        protected override void Update()
+        {
+            base.Update();
+
+            msteerInput = 1;
+            mthrottleInput = 0.5f;
+        }
+    }
+}
